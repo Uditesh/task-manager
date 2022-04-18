226405ec4c2b399d656bb56b3f4fcca34c5883fc
Uditesh <uditesh24jha@gmail.com>
2022-04-18 13:59:49 -0400

    minlength changed

--- backend/database/models/list.js
+++ backend/database/models/list.js
@@ -3,7 +3,7 @@ const ListSchema = new mongoose.Schema({
   title: {
     type: String,
     trim: true,
-    minlength: 2,
+    minlength: 0,
   },
 });
 const List = mongoose.model("List", ListSchema);
